87e84e9dc8aad2592ef91894f4f7faa01f464c6d
shaamiilll <shamilamiyan@gmail.com>
2025-05-14 23:33:04 +0530

    Updated the code in the admin

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -20,6 +20,9 @@ const Orders = ({ token }) => {
 
       const response = await axios.post(backendUrl + '/api/order/list', {}, { headers: { token } })
       if (response.data.success) {
+        console.log('====================================');
+        console.log(response.data.orders);
+        console.log('====================================');
         setOrders(response.data.orders.reverse())
       } else {
         toast.error(response.data.message)
@@ -56,8 +59,10 @@ const Orders = ({ token }) => {
       <div>
         {
           orders.map((order, index) => (
+       
             <div className='grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700' key={index}>
-              <img className='w-12' src={assets.parcel_icon} alt="" />
+              <img className='w-12' src={order.items[0]?.image?.[0]} alt="" />
+
               <div>
                 <div>
                   {order.items.map((item, index) => {
